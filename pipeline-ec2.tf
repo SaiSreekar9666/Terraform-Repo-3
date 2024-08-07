223d9b85a052d3d7c4307d0d0a20a300fcd3066f
SaiSreekar9666 <gsaisreekar9666@gmail.com>
2024-08-07 15:23:56 +0530

    fixed

---
+++ pipeline-ec2.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  region = "us-east-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  
+}
+
+
+
+resource "aws_instance" "name" {
+    key_name = "docker.pem"
+    ami = "ami-0a0e5d9c7acc336f1"
+    instance_type = "t2.micro"
+    tags = {
+      Name = "Server-1"
+    }
+  
+}
+
